9bfecfc7eb87d203f6646c6df7235f848d058c79
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2023-04-26 22:17:49 -0300

    repo inicial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Clase 5
+
+## Ejecutar
+
+- npm i
+
+- docker-compose up -d
+
+## Base datos
+
+- Nombre: api
+
+- Puerto: 3310
+
+- Usuario: root
+
+- Clave: root
---
+++ models/carrera.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const carrera = sequelize.define(
+    'carrera',
+    {
+      nombre: DataTypes.STRING,
+    },
+    {},
+  );
+
+  return carrera;
+};
